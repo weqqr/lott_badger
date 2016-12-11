7c48c77ef5e60417828cc9a294bed72684507d5e
Badger <badger2house1@gmail.com>
2016-12-11 20:41:40 +0400

    Add files via upload

---
+++ mods/mobs/spawn.lua
@@ -0,0 +1,7 @@
+-- Spawning parameters
+-- name, nodes, neighbors, min_light, max_light, interval, chance, active_object_count, min_height, max_height, day_toggle
+
+-- SPIDERS
+mobs:spawn_specific("mobs:tarantula", {"lottmapgen:mirkwood_grass", "default:jungletree"}, {"air"}, 0, 14, 120, 5000, 1, -31000, 31000)
+
+mobs:spawn_specific("mobs:bee", {"group:flower"}, {"air"}, 10, 20, 30, 5000, 1, 0, 1000)
